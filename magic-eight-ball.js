320f152eb925647391449e441afaf849ab6aecbd
Rebecca Tague <rebecca.l.tague@hotmail.co.uk>
2019-02-10 17:54:37 +0000

    First commit

---
+++ magic-eight-ball.js
@@ -0,0 +1,38 @@
+let userName = 'Rebecca';
+console.log(`Hello, ${userName}`);
+
+let userQuestion = 'Is Fang the best cat in the world?';
+console.log(`${userName} asked, ${userQuestion}`);
+
+let randomNumber = Math.floor(Math.random() * 8);
+
+let eightBall = '';
+
+switch (randomNumber) {
+    case 0:
+    eightBall  = 'It is certain';
+    break;
+    case 1:
+    eightBall = 'It is decidedly so';
+    break;
+  	case 2:
+    eightBall = 'Reply hazy try again';
+    break;
+  	case 3:
+    eightBall = 'Cannot predict now';
+    break;
+  	case 4:
+    eightBall = 'Do not count on it';
+    break;
+ 		case 5:
+    eightBall = 'My sources say no';
+		break;
+  	case 6:
+    eightBall = 'Outlook not so good';
+    break;
+  	case 7:
+    eightBall = 'Signs point to yes';
+
+}
+
+console.log(eightBall);
